0349679d8386c0fd6825cc9995037ffb4c5a184c
Aaron Strouse <aaronstrouse@gmail.com>
2017-04-15 21:45:16 -0400

    asynchronous test using done variable in callback function

--- test/tools-spec.js
+++ test/tools-spec.js
@@ -3,14 +3,28 @@ var expect = require("chai").expect
 //Module where printName function will live
 var tools = require("../lib/tools");
 
+describe("Tools", function() {
 
-describe("printName()", function() {
-    it("should print the last name first", function() {
-        var results = tools.printName({ first: "Aaron", last: "Strouse" });
-        expect(results).to.equal("Strouse, Aaron");
+    describe("printName()", function() {
+        it("should print the last name first", function() {
+            var results = tools.printName({ first: "Aaron", last: "Strouse" });
+            expect(results).to.equal("Strouse, Aaron");
+        });
     });
-});
 
+    //This will be an asynchronous test, since the callback behaves asynchronously.
+    //A variable "done" should be passed to the callback function.
+   //So "done" needs to be invoked when the callback is finished.
+    describe("loadWiki()", function() {
+        it ("should load Abe Linconln's wikipedia page", function(done) {
+            tools.loadWiki({ first: "Abraham", last: "Linconln" }, function(html) {
+                expect(html).to.be.ok;
+                done();
+            });
+        });
+    });
+
+});
 
 
 
